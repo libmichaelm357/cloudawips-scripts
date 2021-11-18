1bab584997d830b278206dbf4fc62dfe67a18281
skSpud <68549366+skspuddd@users.noreply.github.com>
2021-11-18 15:51:19 -0500

    Update cloudawips_cos.sh

--- cloudawips_cos.sh
+++ cloudawips_cos.sh
@@ -9,8 +9,36 @@ echo "recommend using CentOS 7 only."
 echo "********** WARNING! **********"
 echo " "
 
-sleep 3
+sleep 3 # Pause before installation process
 
 echo "Beginning the Docker Installation/Update..."
 sudo service docker stop
-sudo yum install docker
+sudo yum install docker # Install Docker if not installed already
+sudo yum update docker # Update Docker if already installed
+sudo service docker start
+echo "Now attempting to print Docker Version Info..."
+sudo docker version
+echo " "
+
+sleep 3 # Pause before docker pull
+
+echo "-- The installer will now begin pulling the latest CloudAWIPS image. --"
+echo "A high-speed network connection is recommended as a slow-speed connection"
+echo "could result in download times of 3 hours or more. You will have 30 seconds"
+echo "to switch to a high-speed connection before the download starts."
+echo "-- The installer will now begin pulling the latest CloudAWIPS image. --"
+echo " "
+sleep 10
+echo "*** 20 SECONDS REMAINING TO SWITCH TO HIGH-SPEED NETWORK BEFORE DOWNLOAD ***"
+sleep 10
+echo "*** 10 SECONDS REMAINING TO SWITCH TO HIGH-SPEED NETWORK BEFORE DOWNLOAD ***"
+sleep 10
+echo " "
+echo "--- Begin pulling of latest CloudAWIPS Image from Docker Hub ---"
+echo "The system will now begin pulling the latest CloudAWIPS Image from Docker."
+echo "Interrupting this process could result in data loss. Please note that you"
+echo "may experience download times of upwards of 1-3+ hours depending on your"
+echo "network speed."
+echo "--- Begin pulling of latest CloudAWIPS Image from Docker Hub ---"
+sleep 3
+sudo docker pull unidata/cloudawips
