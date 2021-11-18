8e466147a1bddf7b8a8fc779e7b09c912f0418ea
skSpud <68549366+skspuddd@users.noreply.github.com>
2021-11-18 15:40:20 -0500

    Update cloudawips_cos.sh

--- cloudawips_cos.sh
+++ cloudawips_cos.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+echo "Please wait, attempting to begin the CloudAWIPS installation."
+echo " "
+echo "********** WARNING! **********"
+echo "This installation script has been tested to function on"
+echo "CentOS 7. While earlier/later versions may work, we highly"
+echo "recommend using CentOS 7 only."
+echo "********** WARNING! **********"
+echo " "
+
+sleep 3
+
+echo "Beginning the Docker Installation/Update..."
+sudo service docker stop
+sudo yum install docker
